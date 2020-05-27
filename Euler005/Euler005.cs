c4f3b9b0210fe827eca79565047d544e7f30cccf
Sean Donoghue <sean.donoghue@pm.me>
2020-05-27 23:35:10 +0100

    Added solution to problem #005

---
+++ Euler005/Euler005.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Euler005
+{
+    class Euler005
+    {
+        static bool Divisible1To20(int number)
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                if (number % i != 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            int number = 20;
+            while (Divisible1To20(number) == false)
+            {
+                number++;
+            }
+            Console.WriteLine(number);
+        }
+    }
+}
