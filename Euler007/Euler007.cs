ef07d3a495ac0329c8c4bf18262e750f676f91a0
Sean Donoghue <sean.donoghue@pm.me>
2020-05-27 23:51:19 +0100

    Added solution to problem #007

---
+++ Euler007/Euler007.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Euler007
+{
+    class Euler007
+    {
+        static bool IsPrime(long number)
+        {
+            if (number == 1)
+            {
+                return false;
+            }
+
+            for (long i = 2; i <= number / 2; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            int primesFound = 0;
+            int currentNumber = 0;
+            while (primesFound < 10001)
+            {
+                currentNumber++;
+                if (IsPrime(currentNumber))
+                {
+                    primesFound++;
+                }
+            }
+            Console.WriteLine(currentNumber);
+        }
+    }
+}
