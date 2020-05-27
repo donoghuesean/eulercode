3f7af8139ec3a9b320467659f13745619348aca2
Sean Donoghue <sean.donoghue@pm.me>
2020-05-27 23:43:54 +0100

    Added solution to problem #006

---
+++ Euler006/Euler006.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Euler006
+{
+    class Euler006
+    {
+        static void Main(string[] args)
+        {
+            int sumOfSquares = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                sumOfSquares += i * i;
+            }
+
+            int squareOfSum = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                squareOfSum += i;
+            }
+            squareOfSum *= squareOfSum;
+
+            int sumSquareDifference = squareOfSum - sumOfSquares;
+
+            Console.WriteLine(sumSquareDifference);
+        }
+    }
+}
