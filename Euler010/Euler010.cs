732fc8be10745be37679d25a1313cb9007361e1b
Sean Donoghue <sean.donoghue@pm.me>
2020-05-28 16:31:12 +0100

    Added solution to problem #010

---
+++ Euler010/Euler010.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Euler010
+{
+    class Euler010
+    {
+        static void SieveOfEratosthenes(List<int> numberList)
+        {
+            int listIndex = 0;
+            int currentSquare = 0;
+            int currentNumber;
+            while (currentSquare < numberList.Last())
+            {
+                currentNumber = numberList[listIndex];
+                currentSquare = currentNumber * currentNumber;
+                numberList.RemoveAll((int x) => (x >= currentSquare) && (x % currentNumber == 0));
+                listIndex++;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int maxNumber = 2000000;
+
+            var numberList = new List<int>();
+            for (int i = 2; i <= maxNumber; i++)
+            {
+                numberList.Add(i);
+            }
+
+            SieveOfEratosthenes(numberList);
+
+            long answer = 0;
+            foreach (int primeNumber in numberList)
+            {
+                answer += primeNumber;
+            }
+
+            Console.WriteLine(answer);
+        }
+    }
+}
