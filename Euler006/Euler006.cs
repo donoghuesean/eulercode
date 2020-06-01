e5cef41cc0b2a8a5047ee34d248f24eac052021d
Sean Donoghue <sean.donoghue@pm.me>
2020-06-01 21:13:24 +0100

    Cleaned up solution to problem #006, use only one for loop

--- Euler006/Euler006.cs
+++ Euler006/Euler006.cs
@@ -12,16 +12,14 @@ namespace Euler006
         static void Main(string[] args)
         {
             int sumOfSquares = 0;
+            int squareOfSum = 0;
+
             for (int i = 1; i <= 100; i++)
             {
                 sumOfSquares += i * i;
-            }
-
-            int squareOfSum = 0;
-            for (int i = 1; i <= 100; i++)
-            {
                 squareOfSum += i;
             }
+
             squareOfSum *= squareOfSum;
 
             int sumSquareDifference = squareOfSum - sumOfSquares;
