d722d4f7652742967ef50012e7721ad6721850d3
Sean Donoghue <sean.donoghue@pm.me>
2020-06-02 17:39:59 +0100

    Added solution to problem #019

---
+++ Euler019/Euler019.cs
@@ -0,0 +1,31 @@
+﻿/* Counting Sundays
+ * Problem 19
+ * https://projecteuler.net/problem=19
+ */
+
+using System;
+
+namespace Euler019
+{
+    class Euler019
+    {
+        static void Main(string[] args)
+        {
+            int totalSundays = 0;
+
+            for (int year = 1901; year <= 2000; year++)
+            {
+                for (int month = 1; month <= 12; month++)
+                {
+                    DateTime date = new DateTime(year, month, 1);
+                    if (date.DayOfWeek == 0)
+                    {
+                        totalSundays++;
+                    }
+                }
+            }
+
+            Console.WriteLine(totalSundays);
+        }
+    }
+}
